c9144eea6e5ba93023199c2967ab7e3510391e3d
Adele Teoh <vagrant@vagrant.vm>
2021-04-13 05:13:03 +0000

    Create search route.

--- server.py
+++ server.py
@@ -3,12 +3,15 @@ Single-Page Web App to search and nominate movies for Shoppies Awards.
 """
 
 # import flask, jinja & session requirements
-from flask import Flask, render_template, session, flash 
+from flask import Flask, render_template, request, session, flash 
+import os, requests
 
 # initialise flask app
 app = Flask(__name__)
 app.secret_key = "temp-dev-skey"
-# set secret key 
+
+OMDB_KEY = os.environ["OMDB_KEY"]
+
 
 # ========= APP ROUTES ==========
 
@@ -19,10 +22,23 @@ def index():
 
     return render_template("index.html")
 
+
 # search
+@app.route("/search")
+def title_search():
+    """Search OMDB API using form input."""
 
+    # get movie title from form 
+    search_term = request.args.get("title-search")
+    print(f"searching omdb for {search_term}")
 
-# marquee? 
+    # send search request to omdb api 
+    search_response = requests.get(f"http://www.omdbapi.com/?apikey={OMDB_KEY}"\
+        f"&s={search_term}")
+    search_res_dict = search_response.json()
+
+    return search_res_dict 
+
 
 # ========== RUN APP ==========
 
