1bf5fa636920c4bbc889fd31bf8148a7da12656d
Adele Teoh <vagrant@vagrant.vm>
2021-04-14 04:29:42 +0000

    Add nominate and remove nomination functionality.

--- server.py
+++ server.py
@@ -3,8 +3,8 @@ Single-Page Web App to search and nominate movies for Shoppies Awards.
 """
 
 # import flask, jinja & session requirements
-from flask import Flask, render_template, request, session, flash 
-import os, requests
+from flask import Flask, render_template, make_response, request, session, flash 
+import os, requests, pprint
 
 # initialise flask app
 app = Flask(__name__)
@@ -20,7 +20,25 @@ OMDB_KEY = os.environ["OMDB_KEY"]
 def index():
     """Return main page for Shoppies.""" 
 
-    return render_template("index.html")
+    # session.clear() 
+    nom_movies = {}
+    
+    # if session has noms, get nominated movie info 
+    if session.get("noms") != None:
+        print("found previous nominations")
+
+        for movie_id in session["noms"]:
+            api_resp = requests.get(f"http://www.omdbapi.com/?apikey={OMDB_KEY}"\
+            f"&i={movie_id}") 
+            api_dict = api_resp.json()
+            nom_movies[movie_id] = {
+                "title": api_dict["Title"],
+                "year": api_dict["Year"]
+            }
+
+        pprint.pprint(nom_movies) 
+        
+    return render_template("index.html", nom_movies=nom_movies)
 
 
 # search
@@ -40,6 +58,44 @@ def title_search():
     search_res_dict = search_response.json()
 
     return search_res_dict
+
+
+@app.route("/nominate")
+def add_nomination():
+    """Add a movie to the nomination list."""
+
+    movie_id = request.args.get("movie_id")
+    print("******** adding nomination to session[noms]:")
+    print(session.get("noms"))
+
+    if session.get("noms") == None:
+        session["noms"] = [movie_id]
+
+    elif len(session.get("noms")) == 5:
+        return "full"
+    
+    else:
+        session["noms"].append(movie_id)
+        session.modified = True     
+        # * needed bec changes on mutable structures not auto picked up 
+        print(f"session noms is now {session['noms']}")
+    
+    return movie_id
+
+
+@app.route("/remove-nom")
+def remove_nomination():
+    """Remove movie from nomination list."""
+
+    movie_id = request.args.get("movie_id")
+    print(f"******** removing {movie_id} from this list:")
+    print(session.get("noms"))
+
+    session["noms"].remove(movie_id)
+    session.modified = True  
+
+    return "ok"
+
 
 
 # ========== RUN APP ==========
--- static/ajax.js
+++ static/ajax.js
@@ -1,12 +1,63 @@
 // ========== Functions ===========
-// search function 
-function searchMovieTitle(evt){};
+
+// --- remove nomination function ---
+function removeNomination(evt) {
+    console.log("remove nomination function called");
+
+    let btn = $(evt.target)
+    let movieId = btn.attr("value"); 
+
+    // send server request to remove from session 
+    $.get("/remove-nom", {"movie_id": movieId}, (res) => {
+        if (res === "ok") {
+            $(`#${movieId}`).remove();
+        }
+    });
+
+    // remove line item from nominated div
+}
+
+
+// --- add nomination function ---
+function addNomination(evt) {
+    console.log("add nomination function called");
+
+    let btn = $(evt.target);
+    let movieId = btn.attr("value"); 
+    let movieInfo = btn.attr("name");
+    
+    // send server request to add to session
+    $.get("/nominate", {"movie_id": movieId}, (res) => {
+        if (res === "full") {
+            alert("oops! please remove a nomination to place another vote");
+        } else {
+            // disable add_button in results list
+            btn.prop("disabled", true);
+
+            // update nominated div
+            console.log("nomination done for");
+            console.log(res);
+
+            let delButton = `<button class="del-nom" value=${movieId} ` + 
+                            `name="${movieInfo}">Remove` + 
+                            `</button>`
+            let newMovieRow = `<li id=${movieId}>${movieInfo} ${delButton}</li>`
+            $("#nom-list").append(`${newMovieRow}`);
+
+            // add event listener to new movie's remove button
+            $("button.del-nom").on("click", (evt) => {
+                removeNomination(evt);
+            });
+        }; 
+    });
+
+};
 
 
 
 // ======= Event Listeners ========
  
-// listen for search 
+// --- Run search and display results ---
 $("#title-search").on("submit", (evt) => {
     evt.preventDefault();
     // gather necessary data 
@@ -28,25 +79,42 @@ $("#title-search").on("submit", (evt) => {
             let movieYear = res.Search[i].Year;
             let movieId = res.Search[i].imdbID;
 
-            let nomButton = `<button id="nom-button" value=${movieId}>Nominate</button>`
+            let nomButton = `<button class="nominate" value=${movieId} ` + 
+                            `name="${movieTitle} (${movieYear})">Nominate` + 
+                            `</button>`
             $("#results-list").append(`<li>${movieTitle} (${movieYear}) ${nomButton}</li>`);
+            
+            // // if movieId is nominated, disable add button
+            // ! fixme
+            // $("#nom-list > li").each( function () {
+            //     let nomId = $(this).attr("id");
+            //     if (movieId === nomId) {
+            //         nomButton.prop("disabled", true);
+            //     };
+            // });
         };
+
+        // *** add event listener to all search results: nomination buttons
+        $("button.nominate").on("click", (evt) => {
+            console.log(evt.target);
+            addNomination(evt);
+        });
     });
 });
 
-// nominate movie
-$("#nom-button").on("click", (evt) => {
-    evt.preventDefault();
+// === nominate movie ===
+$("button.nominate").on("click", (evt) => {
+    // evt.preventDefault();
     // grab relevant data
-    // check total_nominations < 5
+    console.log("clicked!");
+    alert("i see you!");
+
+    // check if total_nominations < 5
     // add movie to session obj OR error dialog
     // update nominated list to add movie
 });
 
 // remove nomination
-$("#rm-nom-button").on("click", (evt) => {
-    evt.preventDefault();
-    // grab relevant data
-    // remove movie from session obj
-    // update nominated list to remove movie 
+$("button.del-nom").on("click", (evt) => {
+    removeNomination(evt);
 });
